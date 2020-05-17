832f98eb08db44b94a335f8e7b6b22154e626cd7
alberes <alberes.menezes@gmail.com>
2020-05-17 11:40:01 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/meiraonline/MeiraonlineDomainApplication.java
@@ -0,0 +1,13 @@
+package br.com.meiraonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeiraonlineDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeiraonlineDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meiraonline/MeiraonlineDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meiraonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeiraonlineDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
